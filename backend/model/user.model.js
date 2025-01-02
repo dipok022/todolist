c9c136be16881ca7156a2097e5a899e85d7bd7ad
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-01-03 01:50:23 +0600

    user schema added

---
+++ backend/model/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    maxlength: 20,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 20,
+    match:
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
