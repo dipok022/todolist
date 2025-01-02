5b53f06d58ba8a2f76d1501d7efdeacd31be02ca
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-01-03 02:48:54 +0600

    user schema timestamps added

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 100,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      maxlength: 100,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 20,
+      match:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    maxlength: 100,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 20,
-    match:
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
