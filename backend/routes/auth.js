3628263ea7b5a7adfc72d8223d7797a27c9fe0bb
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-01-03 13:36:38 +0600

    login api added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
+var jwt = require("jsonwebtoken");
 const usetSchema = require("../model/user.model");
 
 // get all users
 router.get("/users", async (req, res) => {
-  const allUsers = await usetSchema.find({});
-  res.status(200).json(allUsers);
+  try {
+    const allUsers = await usetSchema.find({});
+    res.status(200).json(allUsers);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 });
 
 // create a new user
@@ -33,6 +39,27 @@ router.post("/register", async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+// login a user
+router.post("/login", async (req, res) => {
+  try {
+    const user = await usetSchema.findOne({});
+    if (!user.email || !user.password) {
+      return res.status(401).json("Invalid email or password");
+    }
+    const token = jwt.sign(
+      { id: process.env.JWT_SECRET },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ message: "Logged in successfully", token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
