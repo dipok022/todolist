e68ade590dabb2205432ec132fea68b024ed4c68
dipok <roydipok022@gmail.com>
2025-01-07 16:56:19 +0600

    layout and react router added

---
+++ frontend/src/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layout/layout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
