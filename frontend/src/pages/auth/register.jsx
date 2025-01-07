bfc2d16c575cab5c545496361c5fe036564eb67e
dipok <roydipok022@gmail.com>
2025-01-07 17:08:04 +0600

    register page added

---
+++ frontend/src/pages/auth/register.jsx
@@ -0,0 +1,11 @@
+const Register = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="w-[400px] border border-spacing-1 rounded-lg pb-6 px-6">
+        <h4 className="text-2xl font-medium text-black py-6">Register</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout/layout";
+import Register from "./pages/auth/register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+  },
+  {
+    path: "/app/register",
+    element: <Register />,
   },
 ]);
