5f8a8476b8b3d772e8bf8a18ec019eb77a0a2cfc
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-01-10 21:46:55 +0600

    cleanup

--- frontend/src/pages/auth/register.jsx
+++ frontend/src/pages/auth/register.jsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
-
 const Register = () => {
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const handleChange = (e) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
-  };
+  // const [userData, setUserData] = useState({
+  //   name: "",
+  //   email: "",
+  //   password: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   setUserData({ ...userData, [e.target.name]: e.target.value });
+  // };
+
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <div className="w-[400px] border border-spacing-1 rounded-lg pb-6 px-6">
         <h4 className="text-2xl font-medium text-black py-6">Register</h4>
         <form action="">
           <div>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name" className="">
+              Name
+            </label>
             <input
               type="text"
               id="name"
               name="name"
-              value={userData.name}
+              // value={userData.name}
               placeholder="Name"
             />
           </div>
